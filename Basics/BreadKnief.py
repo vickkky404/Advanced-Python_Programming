c622f9329978c7ce83c8fe39dd24fc7d40c71328
nalin <nalinikantbastia@gmail.com>
2026-05-19 23:19:03 +0530

    Slicing in python

--- Basics/BreadKnief.py
+++ Basics/BreadKnief.py
@@ -3,3 +3,5 @@ items = ["Meta", "Apple", "Adobe", "Google", "Microsoft"]
 item2 = items[1:3]
 print(item2)
 print(item2)
+
+# this is the concept of slicing and it is used to extract a portion of a List, Tuple, or String. 
